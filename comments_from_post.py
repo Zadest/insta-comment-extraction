cc65be3192756b3bf628a5876dba894fa8f32ed3
Moritz Sebastian Hubert Eßer <messer19@uni-koeln.de>
2022-01-16 22:47:35 +0100

    Update: Post to correct format

--- comments_from_post.py
+++ comments_from_post.py
@@ -5,12 +5,15 @@ from selenium.common.exceptions import TimeoutException
 import time
 
 
+# Fehlerbeispiel : https://www.instagram.com/p/CYv4DvoMjQG/
+
 def timer(func):
     def wrapper(*arg,**kwargs):
         t1 = time.time()
         res = func(*arg,**kwargs)
         t2 = time.time()
         print(func.__name__,"with page:",kwargs['post_URL'],'took',(t2-t1),'seconds')
+        return res
     return wrapper
 
 @timer
@@ -18,63 +21,92 @@ def get_comments_per_post(driver:webdriver,post_URL:str='https://www.instagram.c
     try:
         driver.get(post_URL)
     except TimeoutException as e:
-        print(e)
+        print(">> TimeoutException")
         return None
+    time.sleep(2)
 
+    post = {}
+    post['link'] = post_URL
     try:
         image_element = driver.find_element(By.XPATH, '//article/div/div[1]//img')
         image_element.get_attribute('alt')
         image_element.get_attribute('src')
     except:
         pass
+    
+    post['image_src'] = image_element.get_attribute('src') if image_element else ''
+    post['image_alt'] = image_element.get_attribute('alt') if image_element else ''
 
-    #image_caption = driver.find_element(By.XPATH, '//article/div/div[2]/div/div[2]/div[1]/ul/div/li/div/div/div[2]')
-    #print(image_caption.text)
+    try:     
+        caption = driver.find_element(By.XPATH,'//article//ul/div/li/div/div/div[2]/span')
+    except:
+        pass
+    
+    post['caption'] = caption.text if caption else ''
+
+    try:
+        like_count = driver.find_element(By.XPATH,'//article//section[2]/div/div/a/span')
+    except:
+        pass
+    
+    post['likes'] = like_count.text if like_count else ''
+
     try:
         show_more = driver.find_element(By.XPATH, '//article/div/div[2]/div/div[2]/div[1]/ul/li')
     except:
         show_more = False
+
     while show_more:    
         element_count = len(driver.find_elements(By.XPATH, '//article//ul//li'))
-        show_more.click()
-        time.sleep(1.5)
+        try:
+            show_more.click()
+        except:
+            continue
+        
+        time.sleep(3)
+        
         if element_count == len(driver.find_elements(By.XPATH, '//article//ul//li')) or element_count+1 == len(driver.find_elements(By.XPATH, '//article//ul//li')):
             break
+
         try:
             show_more = driver.find_element(By.XPATH, '//article/div/div[2]/div/div[2]/div[1]/ul/li')
         except:
             break
+
+        
     try:
         reply = driver.find_element(By.XPATH, '//span[contains(text(),"View replies")]')
     except:
         reply = False
+
     while reply:
-        reply.click()
-        time.sleep(0.5)
+        try:
+            reply.click()
+        except:
+            continue
+
+        time.sleep(3)
         try:
             reply = driver.find_element(By.XPATH, '//span[contains(text(),"View replies")]')
         except:
             break
+
     comments = []
     for comment in driver.find_elements(By.XPATH, '//article//ul//li'):
         try:
-            username = comment.find_element_by_xpath('.//h3//span[not(contains(text(),"View reply"))]').text
-            content = comment.find_element_by_xpath('./div/div/div/span').text
-            # print('username:', username)
-            # print('comment:', content)
+            username = comment.find_element(By.XPATH,'.//h3//span[not(contains(text(),"View reply"))]').text
+            content = comment.find_element(By.XPATH,'./div/div/div/span').text
             temp_comment = {}
             temp_comment["username"] = username
             temp_comment["comment"] = content
             try:
-                likes = comment.find_element_by_xpath('.//button[contains(text(),"like")]').text[0]
-                #print('likes:', likes)
+                likes = comment.find_element(By.XPATH,'.//button[contains(text(),"like")]').text[0]
             except:
                 likes = 0
-                #print('likes:', 0)
             temp_comment["likes"] = likes
             comments.append(temp_comment)
-            #print('___')
         except:
             pass
-
-    return comments
+    post['comments'] = comments
+    print('>>',len(comments),"Kommentar[e] extrahiert.")
+    return post
